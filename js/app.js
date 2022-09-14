1091247f6efb02ef4b76c49baff429490a45f581
Muhammad Rana <muhammadrana523@gmail.com>
2022-09-14 11:22:05 +0500

    All Responssiveness done

--- js/app.js
+++ js/app.js
@@ -4,38 +4,32 @@ var menu = document.getElementById("menu");
 var logo = document.getElementById("logo");
 let userTexts = document.getElementsByClassName("user-text");
 let userPics = document.getElementsByClassName("user-pic");
-let checkbtn = document.getElementsByClassName('checkbtn')[0];
-
+let checkbtn = document.getElementsByClassName("checkbtn")[0];
 
 function toggleMenu() {
   subMenu.classList.toggle("open-menu");
 }
 
-
-let nav = document.querySelector('#navbar');
+let nav = document.querySelector("#navbar");
 let ul = document.querySelector("#navbar > ul");
 let lis = document.querySelectorAll("#navbar > ul > li");
-function showMenu() 
-{
-  
-  if ( ul.style.opacity == 1) 
-  {
+function showMenu() {
+  if (ul.style.opacity == 1) {
     //Setting Menu Opacity Back to 0
-    ul.style.opacity = 0;  
+    ul.style.opacity = 0;
+    ul.style.display = "none";
 
     //Setting All LI Tags opacity back to 0
-    document.documentElement.style.setProperty('--li_opacity',0);
-  } 
-  else if ( ul.style.opacity == 0 ) 
-  {
+    document.documentElement.style.setProperty("--li_opacity", 0);
+  } else if (ul.style.opacity == 0) {
     //Setting Menu Opacity Back to 1
     ul.style.opacity = 1;
-    
+    ul.style.display = "block";
+
     //Setting All LI Tags opacity back to 1
-    document.documentElement.style.setProperty('--li_opacity',1);
+    document.documentElement.style.setProperty("--li_opacity", 1);
   }
-  
-  
+
   console.log(lis);
 }
 
@@ -55,7 +49,7 @@ function showReview() {
 window.onscroll = function () {
   if (window.pageYOffset >= menu.offsetTop) {
     navbar.classList.add("sticky");
-    checkbtn.style.marginRight = "40px"; 
+    checkbtn.style.marginRight = "40px";
     logo.src = "images/logo.png";
   } else {
     navbar.classList.remove("sticky");
