4ec9a192bf7be77f5916a473bdca0e9795c6424e
Muhammad Rana <muhammadrana523@gmail.com>
2022-09-19 18:17:56 +0500

    Ready to publish

--- js/slider.js
+++ js/slider.js
@@ -1,35 +1,35 @@
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-const images = document.querySelector(".carousel").children;
-const totalImages = images.length;
-let index = 0;
+// const prev = document.querySelector(".prev");
+// const next = document.querySelector(".next");
+// const images = document.querySelector(".carousel").children;
+// const totalImages = images.length;
+// let index = 0;
 
-prev.addEventListener("click", () => {
-  nextImage("next");
-});
+// prev.addEventListener("click", () => {
+//   nextImage("next");
+// });
 
-next.addEventListener("click", () => {
-  nextImage("prev");
-});
+// next.addEventListener("click", () => {
+//   nextImage("prev");
+// });
 
-function nextImage(direction) {
-  if (direction == "next") {
-    index++;
-    if (index == totalImages) {
-      index = 0;
-    }
-  } else {
-    if (index == 0) {
-      index = totalImages - 1;
-    } else {
-      index--;
-    }
-  }
+// function nextImage(direction) {
+//   if (direction == "next") {
+//     index++;
+//     if (index == totalImages) {
+//       index = 0;
+//     }
+//   } else {
+//     if (index == 0) {
+//       index = totalImages - 1;
+//     } else {
+//       index--;
+//     }
+//   }
 
-  for (let i = 0; i < images.length; i++) {
-    images[i].classList.remove("main");
-  }
-  images[index].classList.add("main");
-  setTimeout(nextImage, 5000);
-}
-setTimeout(nextImage, 5000);
+//   for (let i = 0; i < images.length; i++) {
+//     images[i].classList.remove("main");
+//   }
+//   images[index].classList.add("main");
+//   setTimeout(nextImage, 5000);
+// }
+// setTimeout(nextImage, 5000);
